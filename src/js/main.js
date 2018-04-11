35421a8554460aea93fc5428fd86cd579cb4b665
buyanov <awebuyan@gmail.com>
2018-04-11 17:37:36 +1000

    Adaptiv main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,46 @@
 $(document).ready(function() {
-
+  
 	//Param slick slider
 	$('.slider-list-object').slick({
 		dots: true,
 		arrows: false,
-  	slidesToShow: 3,
-  	slidesToScroll: 1,
-  	// autoplay: true,
-  	autoplaySpeed: 3000,
-  	responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        infinite: true,
-        dots: true,
-        slidesToScroll: 1,
-        autoplay: true,
-  			autoplaySpeed: 3000
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          dots: true,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000
+        }
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 780,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-	});
+    ]
+  });
+  
+  // adaptive menu
+  var menuBtn = $('.top-nav_btn');
+      var menu = $('.top-nav_menu');
+      menuBtn.click(function(){
+        menu.toggleClass('top-nav_menu__active');
+      });
 });
